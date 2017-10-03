f8e54993a97193e20f5c0231ee90001ed5925bd3
Ryan Ositis <ryan.ositis@flukenetworks.com>
2017-10-03 10:21:54 -0700

    Time to do some cleanup. Removing Netscout templates.

--- sites/all/modules/custom/fnet_header_footer/theme/bottom_menu_ntct_block.tpl.php
+++
@@ -1,9 +0,0 @@
-<div class="clear_20px">&nbsp;</div>
-<div id="footer_nav">
-</div>
-
-<div id="copyright" style="width:240px;">
-  &copy;<?php echo date('Y') ?> NETSCOUT. All rights reserved.
-</div>
-
-<div class="clear_1px">&nbsp;</div>
