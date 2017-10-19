60295b91d80a8bb8d473f34b631bc25c4a45bdf6
Ryan Ositis <ryan.ositis@flukenetworks.com>
2017-10-19 09:42:35 -0700

    Updated the Products module.

---
+++ sites/all/modules/custom/products/js/products.js
@@ -0,0 +1,15 @@
+$(function(){
+    var formData = $('#blog-subscription-form').serialize();
+    $("#blog-signup").click(function(){
+        $.ajax({
+            url: 'http://info.flukenetworks.com/index.php/leadCapture/save',
+            type: "POST",
+            data: formData,
+            dataType: "json",
+            success: function(data){
+                alert("Successfull"+data);
+            }
+        });
+        return false;
+    });
+})(jQuery);
