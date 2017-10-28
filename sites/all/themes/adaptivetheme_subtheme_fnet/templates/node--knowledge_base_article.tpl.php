7348e7ddde5cc60d6f21d13b90337b91ddca133a
Gauri Kakade <gauri.kakade@zensar.com>
2017-10-28 15:26:23 +0530

    kb vocab name

--- sites/all/themes/adaptivetheme_subtheme_fnet/templates/node--knowledge_base_article.tpl.php
+++ sites/all/themes/adaptivetheme_subtheme_fnet/templates/node--knowledge_base_article.tpl.php
@@ -3,7 +3,7 @@
 	<!-- START: Breadcrumbs -->
 	<div id="kb-breadcrumbs">										
 		<?php 
-				$tid = $node->taxonomy_vocabulary_14[LANGUAGE_NONE][0]['tid'];
+				$tid = $node->field_knowledge_base[LANGUAGE_NONE][0]['entity']->tid;
 				$term_parents = $tid != "all" ? array_reverse(taxonomy_get_parents_all($tid)) : false; // Build an array of the hierarchical parents of the term 
 		?>	
 		<a href="/knowledge-base" id="bc-link">KB Home</a>
