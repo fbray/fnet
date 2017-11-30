f167f271314ab7d2a09e54416dd9d454ff544da6
Gauri Kakade <gauri.kakade@zensar.com>
2017-11-30 20:55:41 +0530

    FNET Marketo changes

--- sites/all/themes/fnet_responsive/templates/node--fnet_marketo_form.tpl.php
+++ sites/all/themes/fnet_responsive/templates/node--fnet_marketo_form.tpl.php
@@ -1,10 +1,11 @@
 <?php
 	global $language;
+	$currlang = $language->language;
 ?>
 
 <div class="mkto-content-main" class="content clear-block">
   <div id="mkt-prod-banner">
-  	<?php if(!empty($node->field_fnet_mkto_banner_img[LANGUAGE_NONE])): 
+  	<?php if(!empty($node->field_fnet_mkto_banner_img[$currlang])): 
 				print render($content['field_fnet_mkto_banner_img']);
 			else: 
 				print render($content['field_fnet_mkto_banner_html']); 
@@ -19,13 +20,13 @@
           print render($disp_title);
         ?>
       </div>
-		<?php if(!empty($node->field_fnet_mkto_prod_img_left[LANGUAGE_NONE])): ?>
+		<?php if(render($content['field_fnet_mkto_prod_img_left'])): ?>
       <div id="mkt-prod-img-left">
         <?php
           print render($content['field_fnet_mkto_prod_img_left']);       
 				?> 
       </div>
-		<?php elseif(!empty($node->field_fnet_mkto_prod_img_right[LANGUAGE_NONE])): ?>
+		<?php elseif(render($content['field_fnet_mkto_prod_img_right'])): ?>
 			 <div id="mkt-prod-img-right">
         <?php
           print render($content['field_fnet_mkto_prod_img_right']);
@@ -46,7 +47,12 @@
 						print t("Download of this content is not available. Sorry for the inconvenience. Please contact your local Fluke Networks Representative if you feel you have reached this message in error.");
 					}
 					else {
-						print $fnet_marketo_form;
+						if(isset($fnet_marketo_form)){
+							print $fnet_marketo_form;
+						}else{
+							$embed_form = field_view_field('node', $node, 'field_fnet_mkto_form_2_0', array('label'=>'hidden'));
+							print render($embed_form);
+						}
 					}
 				?>
       </div>
