f37bd60a85be6e54c2439a7bf0946f277b5e38cd
Ryan Ositis <rositis@gmail.com>
2018-01-02 00:14:49 -0800

    First Draft of Sales Insider module.

---
+++ sites/all/modules/custom/sales_insider/theme/sales_insider_request_thanks_message.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ryanositis
+ * Date: 1/1/18
+ * Time: 8:30 PM
+ */
+?>
+<p>
+<?php print t('Thank you for requesting access to Sales Insider! Your request will be reviewed by our staff and you will be notified shortly.'); ?>
+</p>
