ab08d34fbd03bb584c2bfcd0a28426f557f372cd
Ryan Ositis <ryan.ositis@flukenetworks.com>
2018-01-26 17:09:14 -0800

    Sales Insider code updates.

---
+++ sites/all/modules/custom/sales_insider/theme/sales_insider_request_thanks_message.tpl.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rositis
+ * Date: 1/26/18
+ * Time: 9:33 AM
+ */
+?>
+<p><?php print t('Thank you for requesting access to Sales Insider! Your request will be reviewed by our staff and you will be notified shortly.'); ?></p>
