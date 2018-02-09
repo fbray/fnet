6921b1b733ddab76ef5198256ebb2a819d16ce5c
Ryan Ositis <ryan.ositis@flukenetworks.com>
2018-02-09 12:46:25 -0800

    Updating the Microsites module to make things a bit more portable.

---
+++ sites/all/modules/custom/microsites/theme/linkwarelive_footer.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rositis
+ * Date: 2/9/18
+ * Time: 11:52 AM
+ */
+?>
+<p class="smaller hide-ca hide-au hide-eu hide-in hide-ln hide-su hide-kr hide-br hide-zh hide-ja hide-ru"><?php print t('Call us about LinkWare Live at:');?></p>
+<p>US: 1-800-283-5853<br />
+<?php print t('International:');?> 1-425-446-5500</p>
