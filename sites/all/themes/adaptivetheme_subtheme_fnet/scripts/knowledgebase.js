ce82199b28031dbf49ad3afe263b2e234a7a4b9c
Gauri Kakade <gauri.kakade@zensar.com>
2018-02-16 20:39:40 +0530

    KB javascript

---
+++ sites/all/themes/adaptivetheme_subtheme_fnet/scripts/knowledgebase.js
@@ -0,0 +1,19 @@
+(function($){
+	var kbwidget = $(".ui-widget-content a.active");
+	if($(".ui-widget-content a").hasClass("active")){
+		var headerindex = kbwidget.closest("div.ui-accordion-content").prev("h3.ui-accordion-header").index();
+		switch(headerindex){
+			case 0:
+				headerindex = headerindex;
+				break;
+			case 2:
+				headerindex = headerindex -1;
+				break;
+			case 4:
+				headerindex = headerindex -2;
+				break;
+		}
+    $(".ui-accordion").accordion('option', 'active' , headerindex); 
+		kbwidget.closest("span.field-content").closest("div.views-field-name").css("background-color","#F5F5F5");
+	}
+})(jQuery);
